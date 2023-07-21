dcd5901b6bd94fe35924cd6c238c03f0996fac6d
ren6699 <ren.1003.onepiece@gmail.com>
2023-07-21 10:54:35 +0900

    メニューUI変更

--- resources/views/main-menu.blade.php
+++ resources/views/main-menu.blade.php
@@ -2,66 +2,62 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             @if(Auth::user()->id === 1)
-            指導員ページ
+            指導員メニュー
             @else
-            生徒用ページ
+            生徒用メニュー
             @endif
         </h2>
     </x-slot>
 
 
     @if(Auth::user()->id === 1)
+    <!-- 指導員用メニュー -->
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <x-input-label value="指導員用メニュー"/>
-            <div class="bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <a href="#">相談フォーム 確認画面</a>
-                </div>
-                <div class="p-6 text-gray-900">
-                    <a href="#">学内予定表 編集画面</a>
-                </div>
-                <div class="p-6 text-gray-900">
-                    <a href="#">体調報告 確認画面</a>
-                </div>
-                <div class="p-6 text-gray-900">
-                    <a href="#">進路報告 確認画面</a>
-                </div>
-                <div class="p-6 text-gray-900">
-                    <a href="#">緊急連絡 編集画面</a>
-                </div>
-                <div class="p-6 text-gray-900">
-                    <a href="#">教員予定表 確認・編集画面</a>
-                </div>
-                <div class="p-6 text-gray-900">
-                    <a href="#">生徒出席状況 確認画面</a>
-                </div>
-                <div class="p-6 text-gray-900">
-                    <a href="#">生徒・指導員登録画面</a>
-                </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelpurple-500 hover:bg-pastelpurple-900 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">相談フォーム 確認画面</a>
+            </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelpurple-500 hover:bg-pastelpurple-900 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">学内予定表 編集画面</a>
+            </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelpurple-500 hover:bg-pastelpurple-900 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">体調報告 確認画面</a>
+            </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelpurple-500 hover:bg-pastelpurple-900 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">進路報告 確認画面</a>
+            </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelpurple-500 hover:bg-pastelpurple-900 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">緊急連絡 編集画面</a>
+            </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelpurple-500 hover:bg-pastelpurple-900 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">教員予定表 確認・編集画面</a>
+            </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelpurple-500 hover:bg-pastelpurple-900 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">生徒出席状況 確認画面</a>
+            </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelpurple-500 hover:bg-pastelpurple-900 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">生徒・指導員登録画面</a>
             </div>
         </div>
-    </div>
+
     @else
+    <!-- 生徒用メニュー -->
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <x-input-label value="生徒用メニュー"/>
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <a href="#">相談フォーム 送信画面</a>
-                </div>
-                <div class="p-6 text-gray-900">
-                    <a href="#">学内予定表 確認画面</a>
-                </div>
-                <div class="p-6 text-gray-900">
-                    <a href="#">出席・体調報告 送信画面</a>
-                </div>
-                <div class="p-6 text-gray-900">
-                    <a href="#">進路報告 送信画面</a>
-                </div>
-                <div class="p-6 text-gray-900">
-                    <a href="#">緊急連絡 確認画面</a>
-                </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelblue-900 hover:bg-pastelblue-500 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">相談フォーム 送信画面</a>
+            </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelblue-900 hover:bg-pastelblue-500 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">学内予定表 確認画面</a>
+            </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelblue-900 hover:bg-pastelblue-500 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">出席・体調報告 送信画面</a>
+            </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelblue-900 hover:bg-pastelblue-500 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">進路報告 送信画面</a>
+            </div>
+            <div class="max-w-4xl my-8 mx-auto bg-pastelblue-900 hover:bg-pastelblue-500 shadow-lg sm:rounded-lg transition">
+                <a href="#" class="block p-6 font-bold text-center text-lg text-gray-700 hover:text-gray-500 transition">緊急連絡 確認画面</a>
             </div>
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,17 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'pastelblue-100': '#a1eafb',
+                'pastelblue-500': '#a1e5fb',
+                'pastelblue-900': '#a1e1fb',
+                'pastelpink-100': '#ffcefa',
+                'pastelpink-500': '#ffcef5',
+                'pastelpink-900': '#ffceea',
+                'pastelpurple-100': '#cabbe9',
+                'pastelpurple-500': '#cabbe5',
+                'pastelpurple-900': '#cabbfa',
+            },
         },
     },
 
